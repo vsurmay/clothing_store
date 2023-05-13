df1b562ebc5c28e149e23de6b11718ca72c0aed0
vsurmay <vsurmaj510@gmail.com>
2023-05-13 10:29:28 +0300

    aded quantity basket products for basket item

--- src/components/ShopingCardIcon/ShopingCardIcon.tsx
+++ src/components/ShopingCardIcon/ShopingCardIcon.tsx
@@ -5,14 +5,17 @@ import { Link } from "react-router-dom";
 import { useAppSelector } from "../../redux/hooks";
 
 const ShopingCardIcon: React.FC = () => {
-  const sumBasketProducts = useAppSelector(
-    (state) => state.basket.sumBasketProducts
+  const { sumBasketProducts, products } = useAppSelector(
+    (state) => state.basket
   );
 
   return (
     <Link to={"/basket"}>
       <div className={classes.card}>
-        <ShoppingOutlined style={{ fontSize: "20px" }} />
+        <div className={classes.cardIcon}>
+          <ShoppingOutlined style={{ fontSize: "20px" }} />
+          <span className={classes.countProducts}>{products.length}</span>
+        </div>
         <div className={classes.describtion}>
           <span className={classes.text}>Shopping Cart</span>
           <span className={classes.price}>
